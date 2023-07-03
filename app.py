1efdb632d941bc40dc56a3534a9fe502f72c3c5e
shigen <8915633+shigen@user.noreply.gitee.com>
2023-07-03 21:46:11 +0800

    实现文件列表和文件的下载

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import socket
 import time
 
 import qrcode_terminal
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from tqdm import tqdm
 from werkzeug.datastructures import FileStorage
 
@@ -39,6 +39,35 @@ def upload_file():
             time.sleep(1)
             f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
     return render_template('index.html', address=address)
+
+
+@app.route('/list')
+def file_list():
+    # 获取当前目录
+    current_dir = os.getcwd()
+
+    # 获取当前目录下的所有文件和文件夹
+    files = os.listdir(current_dir)
+
+    # 渲染文件列表页面，并传递文件列表数据
+    return render_template('file_list.html', files=files)
+
+
+@app.route('/download/<filename>')
+def download(filename):
+    # 获取当前目录
+    current_dir = os.getcwd()
+
+    # 构造文件路径
+    file_path = os.path.join(current_dir, filename)
+    print(file_path)
+    # 检查文件是否存在
+    if os.path.exists(file_path):
+        # 使用send_from_directory函数实现文件下载
+        return send_from_directory(current_dir, filename, as_attachment=True)
+    else:
+        # 文件不存在时返回404错误
+        return "File not found", 404
 
 
 if __name__ == '__main__':
